adfdb1696d4cb32c3424f904ff6ad87deac04e70
Dale Mowbray <dale.mowbray@mail.mcgill.ca>
2016-11-11 16:21:44 -0500

    #4 cleanup

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="container">
-          <h1>Speakup <small>{this.props.title}</small></h1>
+          <h1>Speakup </h1>
         <ul className="nav navbar-nav">
             <li><Link to="/">Home</Link></li>
             <li><Link to="/register">Register</Link></li>

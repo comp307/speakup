38bee6805c57d43afded829d0b98ab578860780b
Dale Mowbray <dale.mowbray@mail.mcgill.ca>
2016-11-11 12:29:42 -0500

    #1 added initial content

--- components/Main.js
+++ components/Main.js
@@ -16,10 +16,38 @@ class Main extends React.Component {
 
   render() {
     return (
-      <div>
-        Main Page
-        // Put some fancy promo video here
-      </div>
+      <div className="container">
+          <div className="jumbotron">
+            <h2>Ask Questions Anonymously</h2>
+            <p>Join a lecture stream or start your own. Text about app.. </p>
+          </div>
+
+        <div className="row">
+          <div className="col-md-6">
+              <h2>Create Stream</h2>
+              <div className="form-group">
+                <input type="text" className="form-control" id="username" placeholder="Username" />
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control" id="password" placeholder="Password" />
+              </div>  
+          </div>
+
+          <div className="col-md-6">
+            <h2>Join Stream</h2>
+            <div className="form-group">      
+              <input type="text" className="form-control" id="username" placeholder="Username" />
+            </div>
+            <div className="form-group">
+              <input type="password" className="form-control" id="password" placeholder="Password" />
+            </div>  
+            <div className="form-group">
+              <input type="text" className="form-control" id="streamID" placeholder="Stream ID" />
+            </div>  
+          </div>
+          
+        </div>
+      </div>   
     );
   }
 }

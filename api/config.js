263476ac87d7eb169e44d29193d94aa518312c2c
Maksim <maksim.bober@mail.mcgill.ca>
2016-11-18 13:30:45 -0500

    Add authentification and registration api

---
+++ api/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret':'comp307magic',
+    'database':'mongodb://myserver:speackup@ds147167.mlab.com:47167/learnmdb'
+}
--- api/server.js
+++ api/server.js
@@ -1,8 +1,68 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-require('./sockets.js').Socket(http);
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
 
-http.listen(3333, function() {
-  console.log('Listening on *:3333');
+var jwt    = require('jsonwebtoken'); // create and verify tokens
+var config = require('./config'); 
+var User   = require('./models/user');
+
+var port = process.env.PORT || 8080; 
+mongoose.connect(config.database); 
+app.set('superSecret', config.secret); // create variable superSecret and set it to config.secret
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));//logs requests into console
+
+
+//TO-DO:Check against hash of the user
+app.post('/api/auth',function(req, res){
+    //if username is in database and his password matches with the one in database then send webtoken
+    User.findOne({
+        name: req.body.name,
+        password: req.body.password
+    }, function(err, user){
+        if(err) throw err;
+
+        if(!user){
+            res.json({success: false, message: 'Wrong username'});
+        }else{
+            var token = jwt.sign(user, app.get('superSecret'),{expiresIn: "1h"});
+            res.json({success: true, message: 'Enjoy your token!', token: token});
+        }
+    })
 });
+
+//TO-DO: Hash userpassword register
+app.post('/api/reg', function(req, res) {
+
+  var userModel = new User({ 
+    name: req.body.name, 
+    password: req.body.password
+  });
+
+  //If user is not in the database register him
+  User.findOne({
+      name: req.body.name
+  }, function(err, user){
+      if(err) throw error;
+
+      if(user){
+          res.json({success:false, message: 'Username already exists'});
+      }
+      else{
+        userModel.save(function(err) {
+            if (err) throw err;
+            console.log('User saved successfully');
+            res.json({ success: true, message: 'User is registered' });
+        })
+      }
+    });
+
+});
+
+app.listen(port);
+console.log('Server runs at http:'+port);

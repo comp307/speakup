87c01caa6643c6a43eec918ca7be6b6e947066cc
Alex <jr.rus13@gmail.com>
2016-11-21 14:14:35 -0500

    Update production bundler

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,46 +1,50 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var config = {
+const config = {
     devtool: 'source-map',
     entry: [
-        './app.js'
+        './app.js',
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     plugins: [
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
+                'NODE_ENV': JSON.stringify('production'),
+            },
         }),
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
-                warnings: false
-            }
+                warnings: false,
+            },
         }),
-        new ExtractTextPlugin('style.css')
+        new ExtractTextPlugin('style.css'),
     ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 loaders: ['babel'],
-                exclude: /node_modules/
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.jpg$/,
+                loader: 'file-loader',
             },
             {
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract(
                     'style', // The backup style loader
                     'css?sourceMap!sass?sourceMap'
-                )
-            }
-        ]
-    }
+                ),
+            },
+        ],
+    },
 };
 
 module.exports = config;

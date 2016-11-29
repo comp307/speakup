8bb986d1420e2d4bd8fff5add00b113d9c2ace71
Maksim <maksim.bober@mail.mcgill.ca>
2016-11-28 22:50:39 -0500

    Implement Hash for passwords and validate inputs

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
-var cors        = require('cors');
-
-var jwt    = require('jsonwebtoken'); // create and verify tokens
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken'); // create and verify tokens
 var config = require('./config');
-var User   = require('./models/user');
+var User = require('./models/user');
 
 var port = process.env.PORT || 8080;
 
@@ -20,54 +20,112 @@ app.use(bodyParser.json());
 app.use(morgan('dev')); //logs requests into console
 app.use(cors());
 
+//TO-DO:
+//[x] Check against hash of the user
+app.post('/api/auth', function (req, res) {
+    if (!isCorrectEmail(req.body.name)) {
+        res.json({ success: false, message: 'Provide correct email' });
+    } else {
+        User.findOne({
+            name: req.body.name,
+        }, function (err, user) {
+            if (err) throw err;
 
-//TO-DO:Check against hash of the user
-app.post('/api/auth',function(req, res){
-    //if username is in database and his password matches with the one in database then send webtoken
-    User.findOne({
-        name: req.body.name,
-        password: req.body.password
-    }, function(err, user){
-        if(err) throw err;
+            if (!user) {
+                res.json({ success: false, message: 'Wrong username' });
+            } else {
 
-        if(!user){
-            res.json({success: false, message: 'Wrong username'});
-        }else{
-            var token = jwt.sign(user, app.get('superSecret'),{expiresIn: "1h"});
-            res.json({success: true, message: 'Enjoy your token!', token: token});
-        }
-    })
+                bcrypt.compare(req.body.password, user['password'], function (err, isMatch) {
+                    if (err) {
+                        console.log(err);
+                        res.json({ success: false });
+                    }
+
+                    if (isMatch) {
+                        var token = jwt.sign(user, app.get('superSecret'), { expiresIn: "1h" });
+                        res.json({ success: true, message: 'Enjoy your token!', token: token });
+                    } else {
+                        res.json({ success: false, message: 'Wrong password!' });
+                    }
+                });
+            }
+        })
+    }
 });
 
-//TO-DO: Hash userpassword register
-app.post('/api/reg', function(req, res) {
+//TO-DO: 
+//[x] Hash userpassword register
+//[x] Validate inputs
+//Current password policy
+//Password length should be between 8 and 6 characters
+app.post('/api/reg', function (req, res) {
+    if (!isCorrectEmail(req.body.name)) {
+        res.json({ success: false, message: 'Provide correct email' });
+    } else if (req.body.password.length < 8 || req.body.password.length > 16) {
+        res.json({ success: false, message: 'Inadequate length of password' });
+    }
+    else if (req.body.password != req.body.password2) {
+        res.json({ success: false, message: 'Passwords do not match' });
+    }
+    else {
+        User.findOne({
+            name: req.body.name
+        }, function (err, user) {
+            console.log('error after 2');
+            if (err) throw error;
 
-  var userModel = new User({
-    name: req.body.name,
-    password: req.body.password
-  });
+            if (user) {
+                res.json({ success: false, message: 'Username already exists' });
+            }
+            else {
+                //we are saving user and hashing his password
+                bcrypt.genSalt(10, function (err, salt) {
+                    bcrypt.hash(req.body.password, salt, function (err, hash) {
+                        var userModel = new User({
+                            name: req.body.name,
+                            password: hash
+                        });
+                        userModel.save(function (err) {
+                            if (err) {
+                                console.log(err);
+                                res.json({ success: false });
+                            }
+                            console.log('User saved successfully');
+                            res.json({ success: true, message: 'User is registered' });
+                        });
 
-  //If user is not in the database register him
-  User.findOne({
-      name: req.body.name
-  }, function(err, user){
-      if(err) throw error;
-
-      if(user){
-          res.json({success:false, message: 'Username already exists'});
-      }
-      else{
-        userModel.save(function(err) {
-            if (err) throw err;
-            console.log('User saved successfully');
-            res.json({ success: true, message: 'User is registered' });
+                    });
+                });
+            }
         });
-      }
-    });
-
+    }
 });
 
 var server = app.listen(port);
 console.log('Server runs at http:' + port);
 
-require('./sockets.js').Socket(server);
+var stream_id = 0;
+var streams = [];
+require('./sockets.js').Socket(server, streams);
+
+
+function Stream(id) {
+    this.id = id;
+    this.messeges = [];
+    this.users = [];
+}
+
+//something@domain.superdomain is allowed
+//instead of looping can be done using hashmap
+function isCorrectEmail(eml) {
+    if (eml.length > 50) return false;
+    if (!eml.includes('@')) return false;
+    let email = eml.split('@');
+    if (email.length != 2) return false;
+    if (email[1].split('.').length != 2) return false;
+    //tests if domain of the element is allowed
+    return ['gmail.', 'mcgill.', 'mail.', 'hotmail.'].some(function (element, index, array) {
+        return email[1].includes(element);
+    });
+
+}
--- api/sockets.js
+++ api/sockets.js
@@ -1,67 +1,83 @@
 'use strict';
+
+const jwt = require('jsonwebtoken'); // create and verify tokens
+const config = require('./config');
+const streams = [];
+
 /**
  * Listens for new socket connections
  * @param {object} http - reference to http server
  */
+
 function Socket(http) {
   let io = require('socket.io')(http);
-  let streams = [{
-   id: '2',
-   messages: [],
-   users: [],
-  },
-  {
-    id: '5',
-    messages: [],
-    users: [],
-  }];
+  console.log(streams);
+  io.on('connection', function (socket) {
+    let handshakeData = socket.request;
+    let user = handshakeData._query['user'];
+    let streamID = handshakeData._query['streamID'];
+    let token = handshakeData._query['token'];
 
-  io.on('connection', function(socket) {
-      let handshakeData = socket.request;
-      let user = handshakeData._query['user'];
-      let streamID = handshakeData._query['streamID'];
-
-      // If stream ID was not passed create a new stream
-      if (!streamID) {
-        streamID = '123'; // generate a random ID here
+let isNew = streams.hasOwnProperty(streamID);
+      if (isNew) {
         streams.push({
-          id: streamID,
-          messages: [],
-          users: [],
+            id: streamID,
+            messages: [],
+            users: [],
+        });
+      };
+
+
+    if (token) {
+      jwt.verify(token, config.secret, function (err, decoded) {
+        if (err) {
+          //to-do send error to user
+          console.log('error');
+        }
+        else {
+          if (!streamID) {
+            streamID = streams.lenght+1; 
+            streams.push({
+              id: streamID,
+              messages: [],
+              users: [],
+            });
+          }
+        }
+      })
+    }
+  
+  
+  //Optimization for this is to do dictionary
+    streams.forEach(function (stream) {
+      if (stream.id === streamID) {
+        socket.join(streamID);
+        // Add user to stream
+        if (stream.users.indexOf(user) < 0) {
+          console.log(user + ' connected to stream #' + streamID);
+          stream.users.push(user);
+        }
+
+        // Send user list and message list to connected user
+        socket.emit('welcome', stream.messages);
+        io.to(streamID).emit('userList', stream.users);
+
+
+        // Listen for new messages and broadcast them to the room
+        socket.on('newMessage', function (msg) {
+          stream.messages.push(msg);
+          io.to(streamID).emit('newMessage', msg);
+        });
+
+        // Listen for disconnections and remove users from the list
+        socket.on('disconnect', function () {
+          console.log('User ' + user + ' has disconnected!');
+          let index = stream.users.indexOf(user);
+          stream.users.splice(index, 1);
+          io.to(streamID).emit('userList', stream.users);
         });
       }
-
-      // Add socket to a "room"
-      socket.join(streamID);
-
-      streams.forEach(function(stream) {
-        if (stream.id === streamID) {
-          // Add user to stream
-          if (stream.users.indexOf(user) < 0) {
-            console.log(user + ' connected to stream #' + streamID);
-            stream.users.push(user);
-          }
-
-          // Send user list and message list to connected user
-          socket.emit('welcome', stream.messages);
-          io.to(streamID).emit('userList', stream.users);
-
-
-          // Listen for new messages and broadcast them to the room
-          socket.on('newMessage', function(msg) {
-            stream.messages.push(msg);
-            io.to(streamID).emit('newMessage', msg);
-          });
-
-          // Listen for disconnections and remove users from the list
-          socket.on('disconnect', function() {
-            console.log('User ' + user + ' has disconnected!');
-            let index = stream.users.indexOf(user);
-            stream.users.splice(index, 1);
-            io.to(streamID).emit('userList', stream.users);
-          });
-        }
-      });
+    });
   });
 }
 
